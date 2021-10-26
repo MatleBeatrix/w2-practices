c6022aad90688bc1a658012210aec3e30b5cd1cb
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2021-10-26 15:27:36 +0200

    tuesday branch added

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-/*
-function loadEvent() {
-    console.log('the page has loaded');
-
-    document.getElementById("menu-btn").addEventListener("click", function(event){
-        console.log(event.target);
-        //document.getElementById("menu-btn").classList.toggle("clicked");
-        event.target.classList.toggle("clicked");
-    });
-}
-*/
-
-function fastLog(text){
-    console.log(text);
-    return "muhaha";
+function buttonClicked(event){
+    event.target.classList.toggle("clicked"); 
 }
 
 function loadEvent() {
     console.log('the page has loaded');
+
     document.getElementById("menu-btn").addEventListener("click", buttonClicked);
 }
-
-function buttonClicked(event){
-    //console.log(event);
-    console.log(event.target);
-    //console.log(event.target.classList);
-    event.target.classList.toggle("clicked");
-    const fastLogResult = fastLog("Ezt a szöveget szeretném kiloggoltatni");
-    console.log(fastLogResult);
-    const randomText = "Nagyon random";
-    fastLog(randomText);
-    document.getElementById("about").classList.add("clicked-too");
-}
-
-
 window.addEventListener("load", loadEvent);
