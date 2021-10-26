bc20df22a2c867d87e384a48cf8e688a2a0c21a7
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2021-10-26 15:51:09 +0200

    all changed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function buttonClicked(event){
-    event.target.classList.toggle("clicked"); 
+    event.target.classList.toggle("clicked");
+    document.getElementById("nav").classList.toggle("clicked"); 
 }
 
 function loadEvent() {
@@ -8,3 +9,12 @@ function loadEvent() {
     document.getElementById("menu-btn").addEventListener("click", buttonClicked);
 }
 window.addEventListener("load", loadEvent);
+
+//body kapja a class nevet, de működjön ugyanúgy
+
+/*
+function buttonClicked(event){
+    document.getElementById("bodyID").classList.toggle("clicked"); 
+}
+*/
+
